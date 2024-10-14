37d7de46cdb318fa447658f7e939401f47b552d7
jozef-dudas <dudo2k3@gmail.com>
2024-10-14 23:37:16 +0200

    Initial cleanup of Waveshare repo.

--- RaspberryPi_JetsonNano/python/setup.py
+++ RaspberryPi_JetsonNano/python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 dependencies = ['Pillow']
 
-if os.path.exists('/sys/bus/platform/drivers/gpiomem-bcm2835'):
+if os.path.exists('/sys/bus/platform/drivers/rpi-gpiomem'):
     dependencies += ['RPi.GPIO', 'spidev']
 elif os.path.exists('/sys/bus/platform/drivers/gpio-x3'):
     dependencies += ['Hobot.GPIO', 'spidev']
